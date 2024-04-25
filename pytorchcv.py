d349171b85d8c75769222ff8b077c3da29fa9d15
Yeongbin99 <68084426+Yeongbin99@users.noreply.github.com>
2024-04-25 00:13:43 +0000

    initial commit

--- pytorchcv.py
+++ pytorchcv.py
@@ -10,16 +10,16 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import numpy as np
 
-# GPU 사용 없다면 CPU
+# GPU 사용 없다면 CPU 사용
 default_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # Fashion MNIST 데이터셋을 불러와서 처리하는 과정
 # builtins 모듈을 통해 전역 변수로 설정된 data_train, data_test, train_loader, test_loader가 생성되어 어디서든 접근할 수 있게 됩니다.
-def load_mnist(batch_size=64): # load_mnist라는 이름의 함수를 정의하고, 이 함수는 기본적으로 batch_size 매개변수를 64로 설정합니다. 이 매개변수는 데이터를 얼마나 많은 단위로 나눌지 결정
-    builtins.train_data = torchvision.datasets.FashionMNIST('./data', download=True, train=True, transform=ToTensor()) # torchvision 라이브러리의 datasets 모듈을 사용하여 MNIST 데이터셋을 불러옵니다. './data'는 데이터셋이 저장될 경로를 지정하며, download=True는 해당 경로에 데이터가 없을 경우 인터넷에서 자동으로 다운로드하도록 설정합니다. train=True는 학습용 데이터셋을 불러오는 것을 의미하고, transform=ToTensor()는 데이터셋의 이미지들을 파이토치 텐서로 변환하는 함수를 적용
+def load_mnist(batch_size=64): # load_mnist라는 이름의 함수를 정의하고, batch_size 매개변수는 데이터를 얼마나 많은 단위로 나눌지 결정
+    builtins.train_data = torchvision.datasets.FashionMNIST('./data', download=True, train=True, transform=ToTensor()) # torchvision 라이브러리의 datasets 모듈을 사용하여 Fashion MNIST 데이터셋을 불러옵니다. './data'는 데이터셋이 저장될 경로를 지정하며, download=True는 해당 경로에 데이터가 없을 경우 인터넷에서 자동으로 다운로드하도록 설정합니다. train=True는 학습용 데이터셋을 불러오는 것을 의미하고, transform=ToTensor()는 데이터셋의 이미지들을 파이토치 텐서로 변환하는 함수를 적용
     builtins.test_data = torchvision.datasets.FashionMNIST('./data', download=True, train=False, transform=ToTensor()) # 테스트 데이터셋을 불러오는 코드입니다. train=False로 설정하여 학습용이 아닌 테스트용 데이터셋을 불러옴
-    builtins.train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size) # 학습 데이터셋을 데이터 로더에 로드합니다. 데이터 로더는 데이터셋을 지정된 배치 크기에 맞게 나누고, 이를 반복 가능한 객체로 만들어 학습 과정에서 쉽게 사용할 수 있게 도움
+    builtins.train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size) # 데이터셋을 데이터 로더에 로드합니다. 데이터 로더는 데이터셋을 지정된 배치 크기에 맞게 나누고, 이를 반복 가능한 객체로 만들어 학습 과정에서 쉽게 사용할 수 있게 도움
     builtins.test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
 
 
